1f8312a49ccf02c1f6ff999ec150abaad480a388
NachinBombin <solidnachox@gmail.com>
2026-05-07 20:09:44 -0400

    Delete lua directory

--- lua/entities/ent_ac47_m134_bullet/shared.lua
+++
@@ -1,4 +0,0 @@
-ENT.Type      = "anim"
-ENT.Base      = "base_gmodentity"  -- FIX BUG 3: base_entity is a C++ base, not a valid GMod scripted-entity base
-ENT.PrintName = "AC-47 M134 Bullet"
-ENT.Spawnable = false
